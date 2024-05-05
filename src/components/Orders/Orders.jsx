1ac2101bd02a28786e53845c55c02e1aaaab0b96
hafijure <hafijure@gmail.com>
2024-05-05 17:47:42 +0600

    cart item delete from database done

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
+
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const saveCart = useLoaderData();
+    const [cart, setCart] = useState(saveCart);
+    
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+  
+        
+
+    }
     
     return (
         <div className='shop-container'>
@@ -13,6 +25,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart={handleRemoveFromCart}
                     ></ReviewItem>)
                 }
             </div>
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './ReviewItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faTrashCan} from '@fortawesome/free-solid-svg-icons';
 
-const ReviewItem = (Props) => {
-    const {id, name, img, price} = Props.product;
-    console.log(Props.product);
+const ReviewItem = ({product, handleRemoveFromCart}) => {
+    const {id, name, img, price, quantity} = product;
+    
     return (
         <div className='review-item'>
             <div className='product-thum'>
@@ -15,15 +16,16 @@ const ReviewItem = (Props) => {
                 <div>
                     <p>{name}</p>
                     <p>Price: <span style={{color:'#FF9900'}}>${price}</span> </p>
-                    <p>Shipping Charge:  <span style={{color:'#FF9900'}}>$5</span></p>
+                    <p>Order quantity:  <span style={{color:'#FF9900'}}>{quantity}</span></p>
                 </div>
                 <div>
-                    <button className='delete-icon'><FontAwesomeIcon icon={faTrashCan} /></button>
+                    <button className='delete-icon' onClick={()=>handleRemoveFromCart(id)}><FontAwesomeIcon icon={faTrashCan} /></button>
                 </div>
             </div>
             
         </div>
     );
 };
+
 
 export default ReviewItem;
